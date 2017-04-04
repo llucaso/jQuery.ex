dcec2431275c62896a4ae4b5c93e3b10753d6a97
Lukasz Redzinski <Lukasz Redzinski>
2017-04-04 20:29:29 +0200

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(function(){
+
+	setInterval(function() {
+		var carouselList = $("#carousel ul");
+		$(carouselList).animate({'margin-left': '-=400px'}, 1000); 300, moveFirstSlide});
+
+	function moveFirstSlide() {
+ 		var firstItem = carouselList.find("li:first");
+ 		var lastItem = carouselList.find("li:last");
+ 		lastItem.after(firstItem);
+ 		carouselList.css({marginLeft:0});
+ 	}
+	
+});
+
+
+
+
+
