19cc5fa91ef955865989c79b0cf8c08ade219c1f
Lukasz Redzinski <Lukasz Redzinski>
2017-04-06 22:46:06 +0200

    reverse button in progres

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 ﻿$(function(){
-
+	
+	var buttonReverse = $("#Reverse"); // nie wiem czy potrzebna mi a zmienna
 	var carouselList = $("#carousel ul");
 	setInterval(slide, 1000);
 
 	function slide() {
-		$(carouselList).animate({'marginLeft': '-400px'}, 300, moveFirstSlide);
+		$(carouselList)
+			.click
+			.animate({'marginRight': '-400px'}, 300, moveFirstSlide);
+
 	}
 
 	function moveFirstSlide() {
@@ -13,7 +17,26 @@
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
+
+	function moveFirstSlideReverse() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.after(lastItem);
+		carouselList.css({marginLeft:0});
+	}
 	
+	
+	
+	
+				
+
+
+
+
+
+	
+
+
 });
 
 
