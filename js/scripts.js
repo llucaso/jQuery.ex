843c30da8affab1e7383df92a02dd3c5890906ca
llucaso <lredzinski@gmail.com>
2017-04-07 13:36:47 +0200

    new options in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,43 @@
-﻿$(function(){
+﻿
+
+
+$(function(){
 	
-	var buttonReverse = $("#Reverse"); // nie wiem czy potrzebna mi a zmienna
+	//var buttonReverse = $("#reverse"); // nie wiem czy potrzebna mi a zmienna
 	var carouselList = $("#carousel ul");
 	setInterval(slide, 1000);
-
-	function slide() {
-		$(carouselList)
-			.click
-			.animate({'marginRight': '-400px'}, 300, moveFirstSlide);
-
-	}
 
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-	}
+	};
 
 	function moveFirstSlideReverse() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.after(lastItem);
-		carouselList.css({marginLeft:0});
-	}
+		carouselList.css({marginLeft:0});		//moze -400
+	};
+
+	function slide() {
+		$(carouselList).animate({'marginLeft': '-400px'}, 3000, moveFirstSlide);
+	};
+
+	$('#back').on('click', function slide() {
+    	$(carouselList).animate({'marginLeft': '400px'}, 3000);
+	});
+
+	$('#next').on('click', function slide() {
+    	$(carouselList).animate({'marginRight': '-400px'}, 3000);
+	});
 	
 	
-	
+
+
+
+		
 	
 				
 
@@ -38,7 +49,6 @@
 
 
 });
-
 
 
 
