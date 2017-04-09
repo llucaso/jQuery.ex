8dc9b3b38a390a3e8c13c0f1e470627cfdac469e
llucaso <lredzinski@gmail.com>
2017-04-09 21:45:03 +0200

    buttons works great

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-﻿
-
-
-$(function(){
+﻿$(function(){
 	
 	var carouselList = $("#carousel ul");
-	var interval = setInterval(slide, 1000);   // schowaj to w funkcji!!!
+	var	moveSlidesRight = true;
+	var interval = setInterval(slideToRight, 3000);
 
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
@@ -13,92 +11,44 @@ $(function(){
 		carouselList.css({marginLeft:0});
 	};
 
-	function moveFirstSlideReverse() {
+	function moveLastSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
-		carouselList.css({marginLeft:-400});	// moze -400
-			
+		carouselList.css({marginLeft:-400});
 	};
 
-	function slide() {
-		$(carouselList).animate({'marginLeft': '-400px'}, 300, moveFirstSlide);
+	function slideToRight() {
+		$(carouselList).animate({'marginLeft': '-400px'}, 2000, moveFirstSlide);
+	}
+
+	function slideToLeft() {
+		moveLastSlide();
+		$(carouselList).animate({'marginLeft':'0'}, 2000);
 	}
 
 	$('#next').click(function () {
 		clearInterval(interval);
-		$(carouselList).animate({'marginLeft': '-400px'}, 300, moveFirstSlide);
-		slide ();				//JAK zaladowac setinterval, stop() off() pause()!?
+		slideToRight();
+		interval = setInterval(slideToRight, 3000);
 	});
-
 
 	$('#back').click(function () {
 		clearInterval(interval);
-		$(carouselList).animate({'marginLeft': '400px'}, 300, moveFirstSlideReverse); // back to start? random pic!
-	//					JAK TO RUSZYĆ
+		slideToLeft();
+		interval = setInterval(slideToRight, 3000);
 	});
 
-
-$('#reverse').click(function() {
-		$(carouselList) // switch?
-			.animate({'marginLeft': '400px'}, 1000, moveFirstSlideReverse);
-
-
+	$('#reverse').click(function () {
+		clearInterval(interval);
+		if (moveSlidesRight === true){
+			setInterval(slideToLeft, 3000);
+			moveSlidesRight = false;
+		} else {
+			setInterval(slideToRight, 3000);
+			moveSlidesRight = true;
+		}
 	});
-
-
-
-
-//	$('#run').click(setInterval(function () {
-		
-//		$(carouselList).animate({'marginLeft': '-400px'}, 1000, moveFirstSlide); }, 300);
-
-
-
-//	$('#reverse').click(function (){
-//       clearInterval(interval);
-    //   $(carouselList).stop();
-//    setInterval(slide, 1000);
-//       $(carouselList).animate({'marginLeft': '400px'}, 1000, moveFirstSlide);
-
-
-//        console.log('to jest test reverse');
-//    });
-
-
-
-//	$('#next').click(slide);
-//	$('#back').click(moveFirstSlideReverse);
-
-
-
-/*   $('#reverse').click( function() {
-		$(carouselList)
-
-			.animate({'marginLeft': '400px'}, 1000, moveFirstSlideReverse);
-
-
-	}); 
-
-
-
-	//if ($(buttonReverse).click === true) {
-	//$(buttonReverse).click( function() {console.log('ll');});
-
-//}
-
-
-
-/*
-	$('#back').click(function () {
-    	$(carouselList).animate({'marginLeft': 0}, 3000, moveFirstSlideReverse);
-	});
-
-	$('#next').on('click', function slide() {
-    	$(carouselList).animate({'marginRight': '-400px'}, 3000);
-	});
-*/
-
 });
 
 
